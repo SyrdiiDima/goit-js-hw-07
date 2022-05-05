5b1c175b5b2cd32b7c5c627b26cf0e1e0d5de780
SyrdiiDima <dimasyrdij@icloud.com>
2022-05-05 23:53:17 +0300

    fin

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,101 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const body = document.querySelector('body');
+
+const cardsMarkup = createImageCardsMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', cardsMarkup)
+
+
+
+
+function createImageCardsMarkup(galleryItems) {
+    return galleryItems.map(({preview,original,description}) => {
+        return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `
+    }).join('');
+  
+}
+
+// //----- //----
+
+// gallery.addEventListener('click',onGalleryContainerClick )
+
+
+
+// function onGalleryContainerClick(evt) {
+  
+//   evt.preventDefault();
+
+//   console.log(evt.target);
+//   const isGaleryEl = evt.target.classList.contains('gallery__item');
+//   if (!isGaleryEl) {
+//     return;
+//   }
+//   return evt.target.dataset.original;
+// }
+  
+
+
+const modal = basicLightbox.create(
+	`<div class="modal"><img src="" width="800" height="600"></div>`
+);
+
+gallery.addEventListener('click', onGalleryImageClickOpenModal);
+
+
+function onGalleryImageClickOpenModal(evt) {
+	evt.preventDefault();
+
+	const IMG_TAG = 'IMG';
+	const isEventOnImage = evt.target.nodeName === IMG_TAG;
+	const currentPreviewImage = evt.target;
+	const modalImgRef = modal.element().querySelector('img');
+
+	if (!isEventOnImage) return;
+
+	setOriginalImageURL(currentPreviewImage, modalImgRef);
+  modal.show();
+  
+	window.addEventListener('keydown', onEscapeKeydownCloseModal);
+}
+
+
+function setOriginalImageURL(image, modal) {
+	modal.src = image.dataset.source;
+}
+
+
+function onBackdropClickCloseModal(e) {
+	const IMG_TAG = 'IMG';
+	const isBackdropClicked = e.target.nodeName !== IMG_TAG;
+
+	if (!isBackdropClicked) return;
+
+
+}
+body.removeEventListener('click', onBackdropClickCloseModal);
+
+function onEscapeKeydownCloseModal(e) {
+  const ESC_KEY_CODE = 'Escape';
+  const isEscapeKeyPressed = e.code === ESC_KEY_CODE;
+
+  if (!isEscapeKeyPressed) return;
+
+  window.removeEventListener('keydown', onEscapeKeydownCloseModal);
+
+  modal.close();
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+
+
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', cardsMarkup)
+
+
+function createGalleryCardsMarkup(galleryItems) { 
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" 
+    href="${original}">
+  <img class="gallery__image" src="${preview}" 
+  alt="${description}" />
+</a>`
+    }).join('');
+
+};
+// console.log(gallery);
+
+const lightBox = new SimpleLightbox('.gallery a', {
+
+    captionsData: 'alt',
+    captionDelay: 250,
+
+});
